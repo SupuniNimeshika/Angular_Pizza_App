9c7d33100121cd7a2b0aa38fc86ca7dcbe37aaa3
Your Name <supuni.nimeshika@gmail.com>
2018-01-27 16:14:47 +0530

    Interpolation Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  name = 'Supuni Nimeshika';
+  email = 'supuni.nimeshika@gmail.com';
+  address = {
+    no: 'Weerasewana',
+    city: 'Lebeema',
+    state: 'Matara'
+  };
 }
